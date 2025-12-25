b771e0a20d5a95a66b76b6d5be8139a806abbcff
inikonoff <inikonoff@gmail.com>
2025-12-25 21:14:27 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,21 +3,11 @@ from aiogram import Dispatcher
 from .common import register_common_handlers
 from .recipes import register_recipe_handlers
 from .voice import register_voice_handlers
-from .favorites import register_favorites_handlers # <--- ИМПОРТ
+from .favorites import register_favorites_handlers
 
 def register_all_handlers(dp: Dispatcher):
-    """
-    Регистрирует все обработчики в Диспетчере.
-    """
-    
-    # 1. Команды
+    """Регистрирует все обработчики"""
     register_common_handlers(dp)
-    
-    # 2. Избранное
     register_favorites_handlers(dp)
-    
-    # 3. Голос
     register_voice_handlers(dp)
-    
-    # 4. Текст (последний)
     register_recipe_handlers(dp)
